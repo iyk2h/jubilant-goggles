e76e68b28c3d3e1cc1a26734d0a32259c44416e0
youngkyunIm <yee0230@naver.com>
2024-01-01 17:17:15 +0900

    feat: add smart look
    유저의 행동 트레킹을 위한 툴 적용

--- app/_app.js
+++ app/_app.js
@@ -1,5 +1,23 @@
+import Script from "next/script";
 import "./globals.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.smartlook||(function(d) {
+            var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+            var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+            c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+            })(document);
+            smartlook('init', '840e1fe5802bb8834e995bca454870f8053cb9a3', { region: 'eu' });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
