38cb39fe486a1f4c426dd0845ebe1a2dafa93470
youngkyunIm <yee0230@naver.com>
2024-01-01 17:26:57 +0900

    fix: remove start look

--- app/_app.js
+++ app/_app.js
@@ -1,24 +1,5 @@
-import Script from "next/script";
 import "./globals.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Script
-        id="smartlook"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          window.smartlook||(function(d) {
-            var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
-            var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
-            c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
-            })(document);
-            smartlook('init', '840e1fe5802bb8834e995bca454870f8053cb9a3', { region: 'eu' });
-          `,
-        }}
-      />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
