e6ea400f89b2c5d4f46b73a454301d7a82fb66a2
youngkyunIm <yee0230@naver.com>
2024-01-02 15:19:02 +0900

    fix: 피드백 화면 비율 및 위치 수정

--- app/feedback/page.js
+++ app/feedback/page.js
@@ -2,14 +2,14 @@
 
 export default function FeedBack() {
   return (
-    <>
+    <div className="flex justify-center mb-20">
       <iframe
         src="https://docs.google.com/forms/d/e/1FAIpQLScDXl1lx5x0NBHFuPxjsp7fQC4M_A8ha-Z8XJdzIP3XahA6GA/viewform?embedded=true"
-        width="380"
+        width="100%"
         height="1076"
       >
         로드 중…
       </iframe>
-    </>
+    </div>
   );
 }
