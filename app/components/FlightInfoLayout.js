40f1008b53239a4fcc4c651c0bbe89027271b65b
youngkyunIm <yee0230@naver.com>
2024-01-04 21:24:05 +0900

    refactor: 내역 화면 비율 수정

--- app/components/FlightInfoLayout.js
+++ app/components/FlightInfoLayout.js
@@ -15,17 +15,17 @@ const FlightInfoLayout = ({ flightInfo, onConfirm, text }) => {
       className="bg-gray-100 hover:bg-gray-300 rounded-xl cursor-pointer"
       onClick={onConfirm}
     >
-      <div className="flex justify-between px-2">
-        <div className="text-center">
+      <div className="flex justify-center">
+        <div className="text-center w-2/5">
           <p className="text-5xl">{departureAirportCode}</p>
           <p className="text-xl">{departureCity}</p>
           <p className="text-base mt-1">{departureTime}</p>
         </div>
-        <div className="text-center">
+        <div className="text-center w-1/5">
           <p className="text-3xl mt-2"> ✈︎ </p>
           <p className="text-2xl text-teal-900">{text}</p>
         </div>
-        <div className="text-center">
+        <div className="text-center w-2/5">
           <p className="text-5xl">{arrivalAirportCode}</p>
           <p className="text-xl">{arrivalCity}</p>
           <p className="text-base mt-1">{arrivalTime}</p>
