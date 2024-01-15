8344764a0dcbda543809b225bb7d7a1554f76b72
youngkyunIm <yee0230@naver.com>
2024-01-16 02:00:39 +0900

    feat: 내 여행 목록에 없으면 내 여행 목록에 추가

--- app/[locale]/nap/[key]/page.js
+++ app/[locale]/nap/[key]/page.js
@@ -17,12 +17,17 @@ export default function Nap(param) {
 
   const [airport, setArport] = useState([]);
   const [loadings, setLoadings] = useState(true);
+  const [title, setTitle] = useState();
+  const [isContain, setIsContain] = useState(false);
+  const [isAdded, setIsAdded] = useState(false);
+
   const key = param.params.key;
 
   const getAirportInfo = async () => {
     try {
       const apiUrl = `/api/nap/${key}`;
       const { data } = await axios.get(apiUrl);
+      setTitle(data.title);
       setArport(data.airport);
     } catch (error) {
       console.log(error);
@@ -30,6 +35,40 @@ export default function Nap(param) {
       setLoadings(false);
     }
   };
+
+  const addMyTravel = () => {
+    const storedAirports = localStorage.getItem("nap_results");
+    const parsedAirports = JSON.parse(storedAirports);
+    console.log(title);
+    if (parsedAirports) {
+      const newHistory = [...parsedAirports, { key, title }];
+
+      const sortedHistory = newHistory.sort((a, b) => {
+        if (a.title < b.title) return -1;
+        if (a.title > b.title) return 1;
+        return 0;
+      });
+      localStorage.setItem("nap_results", JSON.stringify(sortedHistory));
+    } else {
+      localStorage.setItem("nap_results", JSON.stringify([{ key, title }]));
+    }
+
+    setIsAdded(true);
+    setIsContain(true);
+
+    setTimeout(() => {
+      setIsAdded(false);
+    }, 2000);
+  };
+
+  useEffect(() => {
+    const storedAirports = localStorage.getItem("nap_results");
+    if (storedAirports) {
+      const parsedAirports = JSON.parse(storedAirports);
+      const isKeyExist = parsedAirports.some((airport) => airport.key === key);
+      setIsContain(isKeyExist);
+    }
+  }, []);
 
   let [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -52,8 +91,6 @@ export default function Nap(param) {
       setLoadings(false);
     }
   }, [airportInfos]);
-
-  useEffect(() => {}, []);
 
   return (
     <>
@@ -85,12 +122,31 @@ export default function Nap(param) {
             ) : (
               <div className=" mb-20">
                 <RecommendNap airportInfos={airport} />
-                <div className="flex justify-end">
-                  <MyButton onClick={openModal} text={t("share")} />
-                  {isModalOpen && (
-                    <ShareLayout value={key} state={true} close={closeModal} />
+                <div className="flex justify-between">
+                  {isContain ? (
+                    <div className="flex justify-center items-center pl-3">
+                      {isAdded && t("added_my_travels")}
+                    </div>
+                  ) : (
+                    <div className="flex items-start">
+                      <MyButton
+                        text={t("add_my_travels")}
+                        onClick={addMyTravel}
+                      />
+                    </div>
                   )}
+                  <div className="flex items-end">
+                    <MyButton onClick={openModal} text={t("share")} />
+                    {isModalOpen && (
+                      <ShareLayout
+                        value={key}
+                        state={true}
+                        close={closeModal}
+                      />
+                    )}
+                  </div>
                 </div>
+                <div className="flex justify-between"></div>
               </div>
             )}
           </>
