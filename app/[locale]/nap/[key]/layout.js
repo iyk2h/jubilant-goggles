39c8d8cdf701698dcc84af7af6ee780e34c7a4d6
youngkyunIm <yee0230@naver.com>
2024-01-17 00:36:11 +0900

    feat: 낮잠 추천받은 결과 공유시 보여지는 metadata 설정

--- app/[locale]/nap/[key]/layout.js
+++ app/[locale]/nap/[key]/layout.js
@@ -1,11 +1,12 @@
 export async function generateMetadata({ params }) {
   const title = params.locale === "ko" ? "시차해결사" : "LagLess";
+  console.log(params);
   const description =
     params.locale === "ko"
       ? "추천 받은 낮잠 시간을 공유합니다. 함께 여행 중 시차를 최소화하세요."
       : "Share the nap time recommended through LagLess. Minimize jet lag together during our travels.";
   return {
-    metadataBase: new URL("https://sky-nap-guide.vercel.app/"),
+    metadataBase: new URL("https://sky-nap-guide.vercel.app/nap/"),
     openGraph: {
       title: title,
       description: description,
