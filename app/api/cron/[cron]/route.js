2ad8fdd676a9e4d5778ae8d5dc035618988e07ef
youngkyunIm <yee0230@naver.com>
2024-02-06 10:42:01 +0900

    refactor: 이메일 전송 테스트

--- app/api/cron/[cron]/route.js
+++ app/api/cron/[cron]/route.js
@@ -1,4 +1,4 @@
-const { findAllByDate, update } = require("../../mongoDB/repository");
+const { findAllByDate } = require("../../mongoDB/repository");
 import { NextResponse } from "next/server";
 import { sendEmail } from "../../mailing/mailingService";
 import { nowDate } from "@/app/utils/DateUtils";
@@ -8,27 +8,13 @@ async function runSchedule() {
   const list = await findAllByDate(curDate);
 
   for (const item of list) {
-    try {
-      await sendEmail({
-        email: item.email,
-        code: item.code,
-        locale: item.locale,
-        departureDate_local_format: item.departureDate_local_format,
-        destination: item.destination,
-      });
-
-      // 메일 전송이 성공한 경우에만 update 함수를 실행합니다.
-      await update({
-        input: {
-          code: item.code,
-          state: item.state,
-          email: item.email,
-          departureDate: item.departureDate,
-        },
-      });
-    } catch (error) {
-      console.error(`Error ${curDate.toISO()} sending email:`, error);
-    }
+    await sendEmail({
+      email: item.email,
+      code: item.code,
+      locale: item.locale,
+      departureDate_local_format: item.departureDate_local_format,
+      destination: item.destination,
+    });
   }
 }
 
